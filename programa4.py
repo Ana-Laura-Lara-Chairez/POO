5657dca72ef1121f290583f8e7542438bf076567
Ana Laura Lara Chairez <analauris2103@gmail.com>
2024-05-19 15:24:01 -0600

    programa 4 - fórmula general

---
+++ programa4.py
@@ -0,0 +1,8 @@
+# Cálculo de x1
+x1 = (-4 + (4**2 - 4*(2 * (-6)))**0.5) / (2*2)
+
+# Cálculo de x2
+x2 = (-4 - (4**2 - 4*(2 * (-6)))**0.5) / (2*2)
+
+print(x1)  # Resultado con el signo más
+print(x2)  # Resultado con el signo menos
